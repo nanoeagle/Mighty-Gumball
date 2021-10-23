2220822f73a7689dc929d3b2a721f49046a471fd
nanoeagle <fistrias.ikl@gmail.com>
2021-10-23 17:25:28 +0700

    Use JSONP instead of XMLHttpRequest to retrieve real data from another separate server without cross-domain issues.

--- js/mightygumball.js
+++ js/mightygumball.js
@@ -1,21 +1,6 @@
 var salesDiv = document.getElementById("sales");
 
-retrieveSalesData();
-
-function retrieveSalesData() {
-    var salesDataURL = "http://localhost:8080/MightyGumball/json/sales.json";
-    var request = new XMLHttpRequest();
-    request.open("get", salesDataURL);
-    request.onload = function() {
-        if (request.status == 200) {
-            updateSales(request.responseText);
-        }
-    };
-    request.send(null);
-}
-
-function updateSales(responseText) {
-    var salesData = JSON.parse(responseText);
+function updateSales(salesData) {
     salesData.forEach(dataItem => {
         presentSalesDataItem(dataItem);
     });
